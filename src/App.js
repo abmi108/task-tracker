15bd30224a93d735946ef07506bf5f0796eebee1
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-11 23:05:03 +0530

    Add delete task functionality

--- src/App.js
+++ src/App.js
@@ -24,10 +24,17 @@ function App() {
 
   const [tasks, setTasks] = useState(taskList);
 
+  const deleteTask = (id) => {
+    console.log(id);
+    setTasks(
+      tasks.filter((task) => task.id !== id )
+    );
+  };
+
   return (
     <div className="App">
       <Header />
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,6 @@
 import { FaTimes } from "react-icons/fa"
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
     return (
         <div className="task">
             <h3 className="task"> {task.text} 
@@ -9,6 +9,7 @@ const Task = ({ task }) => {
                         color: "red", 
                         cursor: "pointer"
                     }}
+                    onClick={() => onDelete(task.id)}
                 /> 
             </h3>
             <p> {task.day} </p>
