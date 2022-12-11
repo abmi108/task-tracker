afc5817614d5daea1ece250d590f5895b4bf5787
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-11 17:08:58 +0530

    Add button component to header component
    button component with defaultProps, propTypes check
    props: color, text, onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = (props) => {
+
+    const onClick = () => {
+        console.log("click");
+    };
+
     return (
-        <header>
+        <header className="header">
             <h1> {props.title} </h1>
+            <Button color="green" text="Add" onClick={onClick}/>
         </header>
     );
 };
