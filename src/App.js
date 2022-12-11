cc3b3523d97ca2cd423d7f055d5ca1c6df621906
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-11 20:45:56 +0530

    Move task state from tasks component to app component
    List of tasks is moved to root component and
    passed as props to tasks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
+const taskList = [
+  {
+      id: 1,
+      text: "Go for shopping",
+      day: "Dec 13th at 1:30pm"
+  },
+  {
+      id: 2,
+      text: "Find hotels",
+      day: "Dec 15th at 10:30am"
+  },
+  {
+      id: 3,
+      text: "Go to temple",
+      day: "Dec 19th at 12:30pm"
+  },
+];
+
 function App() {
+
+  const [tasks, setTasks] = useState(taskList);
+
   return (
     <div className="App">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,5 @@
-import { useState } from "react";
 
-const taskList = [
-    {
-        id: 1,
-        text: "Go for shopping",
-        day: "Dec 13th at 1:30pm"
-    },
-    {
-        id: 2,
-        text: "Find hotels",
-        day: "Dec 15th at 10:30am"
-    },
-    {
-        id: 3,
-        text: "Go to temple",
-        day: "Dec 19th at 12:30pm"
-    },
-];
-
-const Tasks = () => {
-
-    const [tasks, setTasks] = useState(taskList);
+const Tasks = ({ tasks }) => {
 
     return (
         <>
