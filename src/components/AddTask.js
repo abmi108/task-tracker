5e0da13f75123055e9a2da5258752ca644dd49b4
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-12 21:21:54 +0530

    Add state to the input fields in AddTask component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,22 +1,44 @@
+import { useState } from "react";
+
 const AddTask = () => {
+
+    const [ text, setText ] = useState("");
+    const [ day, setDay ] = useState("");
+    const [ reminder, setReminder ] = useState(false);
+
+
     return (
         <form className="add-form">
             <div className="form-control">
                 <label> 
                     Add Task
-                    <input type="text" placeholder="Enter a task" />
+                    <input 
+                        type="text" 
+                        placeholder="Enter a task" 
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                    />
                 </label>
             </div>
             <div className="form-control">
                 <label> 
                     Add Day & Time
-                    <input type="text" placeholder="Enter Day & Time" />
+                    <input 
+                        type="text" 
+                        placeholder="Enter Day & Time"
+                        value={day}
+                        onChange={(e) => setDay(e.target.value)} 
+                    />
                 </label>
             </div>
             <div className="form-control form-control-check">
                 <label> 
                     Set Reminder
-                    <input type="checkbox" />
+                    <input 
+                        type="checkbox"
+                        value={reminder}
+                        onChange={(e) => setReminder(e.currentTarget.checked)} 
+                    />
                 </label>
             </div>
         </form>
