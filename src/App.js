13f0d6b771dbc6451ed760ce6f791fad84cd7739
“Abhijit <hello.abhijitmishra@gmail.com>
2022-12-18 17:27:22 +0530

    Add tasks to json server

--- src/App.js
+++ src/App.js
@@ -9,30 +9,39 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const dataFromServer = fetchTasks();
-    // setTasks(dataFromServer);
-
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
     };
-
     getTasks();
   }, []);
 
 
   // fetch Tasks
   const fetchTasks = async () => {
+    // gives the entire list of tasks. mehod is GET by default 
     const res = await fetch("http://localhost:5000/tasks");
     const data = await res.json();
     return data;
   }
 
   // add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = {id, ...task};
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    // previously used logic to generate random id's for tasks
+    // const id = Math.floor(Math.random() * 10000) + 1;
+
+    // response is the newly added task 
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify(task)
+    });
+
+    const newlyAddedTask = await res.json();
+    setTasks([...tasks, newlyAddedTask]);
+
   };
 
   // toggle reminder
